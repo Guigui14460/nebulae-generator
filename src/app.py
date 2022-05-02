3c4f472854afb154b8d6a3e35e7e49a2754ef5b0
Guigui14460 <letgui2@gmail.com>
2022-05-02 21:11:04 +0200

    fix: add author

--- src/app.py
+++ src/app.py
@@ -20,6 +20,8 @@ st.write("""
 With the single page application, you can create a brand new nebula image (size 128x128 pixels).
 
 The model has been trained with the **TensorFlow** library.
+
+Created by [Guillaume Letellier](https://guillaumeletellier-portfolio.netlify.app/).
 """)
 if st.button("Generate new nebula"):
     image = generate()
